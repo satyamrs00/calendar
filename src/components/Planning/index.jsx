62e283437c70eb92ecdc903b52108c5a6c9a67b5
Satyam Sagar <satyamsagar1711@gmail.com>
2023-05-31 01:04:14 +0530

    dynamic calendar

--- src/components/Planning/index.jsx
+++ src/components/Planning/index.jsx
@@ -13,38 +13,39 @@ const Planning = () => {
         {
             id: 1,
             name: "Period 1",
-            date: new Date("2023-04-10"),
-            start: new Date("2023-04-10 09:00"),
-            end: new Date("2023-04-10 10:00"),
+            date: new Date(2023, 5, 2),
+            start: new Date(2023, 5, 2, 9, 0),
+            end: new Date(2023, 5, 2, 10, 0),
             color: "#D16D9A",
             activities: [ "Activity 1", "Activity 2" ]
         },
         {
             id: 2,
             name: "Period 2",
-            date: new Date("2023-04-10"),
-            start: new Date("2023-04-10 11:00"),
-            end: new Date("2023-04-10 12:00"),
+            date: new Date(2023, 5, 3),
+            start: new Date(2023, 5, 3, 1, 0),
+            end: new Date(2023, 5, 3, 2, 0),
             color: "#938BE6",
         },
         {
             id: 3,
             name: "Period 3",
-            date: new Date("2023-04-10"),
-            start: new Date("2023-04-10 13:00"),
-            end: new Date("2023-04-10 14:00"),
+            date: new Date(2023, 5, 1),
+            start: new Date(2023, 5, 1, 13, 0),
+            end: new Date(2023, 5, 1, 14, 0),
             color: "#E7C546",
         },
         {
             id: 4,
             name: "Period 4",
-            date: new Date("2023-04-10"),
-            start: new Date("2023-04-10 15:00"),
-            end: new Date("2023-04-10 16:00"),
+            date: new Date(2023, 4, 31),
+            start: new Date(2023, 4, 31, 15, 0),
+            end: new Date(2023, 4, 31, 16, 0),
             color: "#02B683",
         },
     ])
     const [addPeriodModal, setAddPeriodModal] = useState(false)
+    const [currentStart, setCurrentStart] = useState(new Date())
 
     const handleSubmit = (newP) => {
         const newPs = [{
@@ -95,11 +96,15 @@ const Planning = () => {
             </TabPanel>
             <TabPanel value={tab} index={1} className="h-full">
                 <div className="flex justify-start items-center p-4">
-                    <div className="text-lg font-bold">April 10-17</div>
+                    <div className="text-lg font-bold">
+                        {`${currentStart?.toLocaleDateString([], {year: 'numeric', month: 'long', day: 'numeric' })} - ${new Date(currentStart?.getTime() + 6 * 24 * 60 * 60 * 1000)?.toLocaleDateString([], {year: 'numeric', month: 'long', day: 'numeric' })}`}
+                    </div>
                     <div className="flex mx-4 gap-2">
-                        <img src={stroke} alt="" className="" />
-                        Today
-                        <img src={stroke} alt="" className=" rotate-180" />
+                        <img src={stroke} alt="" className="cursor-pointer" onClick={() => setCurrentStart(new Date(currentStart?.getTime() - 7 * 24 * 60 * 60 * 1000))} />
+                        <div onClick={() => setCurrentStart(new Date())} className="cursor-pointer">
+                            Today
+                        </div>
+                        <img src={stroke} alt="" className="cursor-pointer rotate-180" onClick={() => setCurrentStart(new Date(currentStart?.getTime() + 7 * 24 * 60 * 60 * 1000))} />
                     </div>
                     <div className="ml-auto">
                         <Button variant="contained" color="primary" disableElevation sx={{ textTransform: "none", borderRadius: 9999 }} onClick={() => setAddPeriodModal(true)}>
@@ -110,11 +115,17 @@ const Planning = () => {
                 </div>
                 <div className="grid grid-cols-[100px_repeat(7,_1fr)] text-center">
                     <div></div>
-                    <div className="flex flex-col justify-center items-center">
-                        <div className="text-xs text-[#5364FF]">MON</div>
-                        <span className="text-2xl bg-[#5364FF] rounded-full px-2 aspect-square flex justify-center items-center text-white">10</span>
-                    </div>
-                    <div className="flex flex-col justify-center items-center">
+                    {[...Array(7)].map((_, i) => (
+                        <div className="flex flex-col justify-center items-center">
+                            <div className={"text-xs" + (currentStart?.getDate() + i === new Date().getDate() ? ' text-[#5364FF]' : '')}>
+                                {new Date(currentStart?.getTime() + i * 24 * 60 * 60 * 1000)?.toLocaleDateString([], {weekday: 'short'}).toUpperCase()}
+                            </div>
+                            <span className={"text-2xl rounded-full px-2 aspect-square flex justify-center items-center" + (currentStart?.getDate() + i === new Date().getDate() ? ' bg-[#5364FF] text-white' : '')}>
+                                {new Date(currentStart?.getTime() + i * 24 * 60 * 60 * 1000)?.toLocaleDateString([], {day: '2-digit'})}
+                            </span>
+                        </div>
+                    ))}
+                    {/* <div className="flex flex-col justify-center items-center">
                         <div className="text-xs">TUE</div>
                         <span className="text-2xl px-2 aspect-square flex justify-center items-center">11</span>
                     </div>
@@ -137,15 +148,10 @@ const Planning = () => {
                     <div className="flex flex-col justify-center items-center">
                         <div className="text-xs">SUN</div>
                         <span className="text-2xl px-2 aspect-square flex justify-center items-center">16</span>
-                    </div>
-                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0"></div>
-                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0"></div>
-                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0"></div>
-                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0"></div>
-                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0"></div>
-                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0"></div>
-                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0"></div>
-                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0 border-r-0"></div>
+                    </div> */}
+                    {[...Array(8)].map((_, i) => (
+                        <div className={'min-h-[3rem] border border-gray-300 border-t-0 border-l-0' + (i === 7 ? ' border-r-0': '')}></div>
+                    ))}
                 </div>
                 <div className="overflow-y-scroll scrollbar-hide" style={{
                     // height: "calc(100% - 13.5rem",
@@ -160,9 +166,9 @@ const Planning = () => {
                                     </div>
                                 </div>
                                 {[...Array(7)].map((_, j) => (
-                                    <div className="min-h-[3rem] border border-gray-300 border-t-0 border-l-0">
+                                    <div className={"min-h-[3rem] border border-gray-300 border-t-0 border-l-0" + (j === 6 && ' border-r-0')}>
                                         <div className="h-full flex justify-center items-stretch">
-                                            {periods?.filter(period => period.start.getHours() === i && period.date.getDate() === 10 + j)?.map(period => (
+                                            {periods?.filter(period => period.start.getHours() === i && period.date.getDate() === new Date(currentStart?.getTime() + j * 24 * 60 * 60 * 1000)?.getDate()).map(period => (
                                                 <PeriodBlock period={period} />
                                             ))}
                                         </div>
